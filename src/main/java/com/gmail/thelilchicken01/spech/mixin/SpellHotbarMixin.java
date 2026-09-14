0f1caf1f93fe183203ef8b1947c455e2d0f106a1
ethancarson <thelilchicken01@gmail.com>
2026-09-14 14:59:35 -0600

    basically removed tag functionality and made the mod clientside only, at least in theory.

--- src/main/java/com/gmail/thelilchicken01/spech/mixin/SpellHotbarMixin.java
+++ src/main/java/com/gmail/thelilchicken01/spech/mixin/SpellHotbarMixin.java
@@ -1,24 +1,19 @@
 package com.gmail.thelilchicken01.spech.mixin;
 
-import com.gmail.thelilchicken01.spech.util.SpECHTags;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.platform.InputConstants;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.spell_engine.api.spell.container.SpellContainer;
 import net.spell_engine.api.spell.container.SpellContainerHelper;
-import net.spell_engine.client.SpellEngineClient;
 import net.spell_engine.client.input.SpellHotbar;
-import net.spell_engine.client.input.WrappedKeybinding;
-import net.spell_engine.mixin.client.control.KeybindingAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@OnlyIn(Dist.CLIENT)
 @Mixin(SpellHotbar.class)
 public class SpellHotbarMixin {
     @Inject(
